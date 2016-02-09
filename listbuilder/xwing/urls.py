2443548b72f01f7e13cfac22469a7d6f735605f3
Nathan Glimsdale <nglimsdale@gmail.com>
2016-02-09 14:51:16 -0600

    url-tags: use url tags
    use url tags instead of hardcoded urls.

--- listbuilder/xwing/urls.py
+++ listbuilder/xwing/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.lists, name='List Index'),
-    url(r'^lists/', views.lists, name='List Index'),
-    url(r'^list/(?P<list_id>[0-9]+)/$', views.list, name=''),
-    url(r'^ships/', views.ships, name='Ship Index'),
-    url(r'^ship/(?P<ship_id>[0-9]+)/$', views.ship, name=''),
+    url(r'^$', views.lists, name='root-page'),
+    url(r'^lists/', views.lists, name='list-index'),
+    url(r'^list/(?P<list_id>[0-9]+)/$', views.list, name='list'),
+    url(r'^ships/', views.ships, name='ship-index'),
+    url(r'^ship/(?P<ship_id>[0-9]+)/$', views.ship, name='ship'),
 ]
